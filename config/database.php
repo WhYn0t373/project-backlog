9bd7ecc1ea936605af4cf5c7895756de78dfd1f1
WhYn0t373 <59360416+WhYn0t373@users.noreply.github.com>
2026-04-23 17:13:44 +0200

    feat: add config/database.php

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    // ...
+
+    'redis' => [
+
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . ':'),
+        ],
+
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
+
+        // Define any additional named connections if necessary
+    ],
+
+    // ...
+];
