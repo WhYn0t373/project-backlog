f4e744395d580808e5eeaa728bda5e3b4c0bf30b
WhYn0t373 <59360416+WhYn0t373@users.noreply.github.com>
2026-04-24 12:27:50 +0200

    feat: update resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-</head>
-<body>
-    <div style="text-align: center; margin-top: 50px;">
-        <h1>Welcome to Laravel 10!</h1>
-        <p>
-            <a href="https://laravel.com/docs/10.x">Documentation</a> |
-            <a href="https://laravel.com/forum">Forum</a> |
-            <a href="https://laracasts.com">Laracasts</a>
-        </p>
-    </div>
+@extends('layouts.app')
 
-    <script src="{{ mix('js/app.js') }}"></script>
-</body>
-</html>
+@section('content')
+<div class="p-4">
+    Welcome to the application!
+</div>
+@endsection
