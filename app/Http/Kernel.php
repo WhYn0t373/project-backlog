a8ab34b68f51fa5d09dd549c39f9108d9ae0c202
WhYn0t373 <59360416+WhYn0t373@users.noreply.github.com>
2026-04-24 11:55:45 +0200

    feat: update app/Http/Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // Existing global middleware...
+        // Global middleware
     ];
 
     /**
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            // Existing web middleware...
+            // Web group middleware
         ],
 
         'api' => [
@@ -34,10 +34,20 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware.
      *
+     * These middleware may be assigned to groups or used individually.
+     *
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        // Existing route middleware...
-        'jwt' => \App\Http\Middleware\JwtMiddleware::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // Role based middleware
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
