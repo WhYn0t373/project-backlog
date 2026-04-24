5921d9fce1b096a85f7863c04a5fc0de1eb92079
WhYn0t373 <59360416+WhYn0t373@users.noreply.github.com>
2026-04-24 15:46:43 +0200

    feat: update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .js('resources/js/conversion.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-   ]);
-
-mix.browserSync('localhost:8000');
+mix
+  .js('resources/js/app.js', 'public/js')
+  .css('resources/css/app.css', 'public/css')
+  .styles(['resources/css/navbar.css'], 'public/css/navbar.css')
+  .sourceMaps();
